a0d07bd55fe1f4bea42a373349495d0e8160a16d
Moksh Verma <32541482+MokshVerma@users.noreply.github.com>
2023-11-22 23:08:36 +0530

    Time: 32 ms (42.57%), Space: 7.1 MB (85.98%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.go
@@ -1,36 +1,31 @@
-type void struct{}
-var null void
 func findDifference(nums1 []int, nums2 []int) [][]int {
-	var ok bool
 	var result [][]int
-	set1 := map[int]void{}
-	set2 := map[int]void{}
+    set1 := map[int]bool{}
+    set2 := map[int]bool{}
 
 	for i := 0; i < len(nums1); i++ {
-		set1[nums1[i]] = null
+		set1[nums1[i]] = true
 	}
 
 	for i := 0; i < len(nums2); i++ {
-		set2[nums2[i]] = null
+		set2[nums2[i]] = true
 	}
 
 	result = append(result, []int{})
 
 	for i := 0; i < len(nums1); i++ {
-		_, ok = set2[nums1[i]]
-		if !ok {
+		if !set2[nums1[i]] {
 			result[0] = append(result[0], nums1[i])
-			set2[nums1[i]] = null
+			set2[nums1[i]] = true
 		}
 	}
 
 	result = append(result, []int{})
 	
 	for i := 0; i < len(nums2); i++ {
-		_, ok = set1[nums2[i]]
-		if !ok {
+		if !set1[nums2[i]] {
 			result[1] = append(result[1], nums2[i])
-			set1[nums2[i]] = null
+			set1[nums2[i]] = true
 		}
 	}
 	return result
