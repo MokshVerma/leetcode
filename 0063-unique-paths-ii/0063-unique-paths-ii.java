94c568cdbfc98a660361cabf99b92c467b4c3911
Moksh Verma <32541482+MokshVerma@users.noreply.github.com>
2024-01-09 23:38:49 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (35.67%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,30 +3,25 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int[][] dp = new int[n][m];
-        for(int i=0; i<m; i++) {
-            if(grid[0][i] == 1){
-                break;
-            }
-            dp[0][i] = 1;
+        
+        if(grid[0][0] == 1) {
+            return 0;
         }
+        dp[0][0] = 1;
         
         for(int i=0; i<n; i++) {
-            if(grid[i][0] == 1){
-                break;
-            }
-            dp[i][0] = 1;
-        }
-        
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                if(grid[i][j] == 0) {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                } else {
-                    dp[i][j] = 0;
+            for(int j=0; j<m; j++) {
+                if(grid[i][j] == 0 && (i > 0 || j > 0)) {
+                    if(i==0) {
+                        dp[i][j] = dp[i][j-1];
+                    } else if (j==0) {
+                        dp[i][j] = dp[i-1][j];
+                    } else {
+                        dp[i][j] = dp[i-1][j]  + dp[i][j-1];
+                    }
                 }
             }
         }
-        
         return dp[n-1][m-1];
     }
 }
