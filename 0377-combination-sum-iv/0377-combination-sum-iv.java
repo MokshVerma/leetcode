6e85bc0a243e1988185f38a3286467cd1a26c3b5
Moksh Verma <32541482+MokshVerma@users.noreply.github.com>
2024-01-10 13:01:31 +0530

    Time: 2 ms (42.25%), Space: 41 MB (18.77%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,14 +1,10 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
         Arrays.sort(nums);
-        Set<Integer> set = new LinkedHashSet<>();
-        for(int num: nums) {
-            set.add(num);
-        }
 
         int[] dp = new int[target+1];
         for(int i=0; i<=target; i++) {
-            for(int num: set) {
+            for(int num: nums) {
                 if(num < i) {
                     dp[i] += dp[i-num];
                 } else if (num == i) {
